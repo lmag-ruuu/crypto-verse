39602039e4f73c80648d8991eb5ba98c89a94adf
lmagruu <magg4ms@gmail.com>
2022-02-06 20:22:42 -0400

    setup project, react-router, routes, and files

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,25 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { FC } from "react";
+import { Outlet, Link } from "react-router-dom";
+import { Layout, Typography, Space } from "antd";
+import { Navbar } from "./components";
+import "./App.css";
 
-function App() {
+const App: FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div className="navbar">
+        <Navbar />
+      </div>
+      <div className="main">
+        <Layout>
+          <div className="routes">
+            <Outlet />
+          </div>
+        </Layout>
+        <div className="footer">{/* <Footer /> */}</div>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
