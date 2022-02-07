898b0e2fd4e9239a1999216aa6660908f4a99eea
lmagruu <magg4ms@gmail.com>
2022-02-06 22:01:52 -0400

    add reducer and store

--- src/components/pages/HomePage.tsx
+++ src/components/pages/HomePage.tsx
@@ -3,10 +3,17 @@ import millify from "millify";
 import { Typography, Row, Col, Statistic } from "antd";
 import { Cryptocurrencies, News } from "../../components";
 import { Link } from "react-router-dom";
+import { useGetCryptosQuery } from "../../services/cryptoApi";
 
 const { Title } = Typography;
 
 const HomePage: FC = () => {
+  const { data, isFetching } = useGetCryptosQuery(10);
+
+  const globalStats = data?.data.stats;
+
+  if (isFetching) return <p>... Loading</p>;
+
   return (
     <main>
       <Title level={2} className="heading">
@@ -17,35 +24,35 @@ const HomePage: FC = () => {
           {" "}
           <Statistic
             title="Total Cryptocurrencies"
-            // value={globalStats.total}
+            value={globalStats.total}
           />{" "}
         </Col>
         <Col span={12}>
           {" "}
           <Statistic
             title="Total Exchanges"
-            // value={millify(globalStats.totalExchanges)}
+            value={millify(globalStats.totalExchanges)}
           />{" "}
         </Col>
         <Col span={12}>
           {" "}
           <Statistic
             title="Total Market Cap"
-            // value={millify(globalStats.totalMarketCap)}
+            value={millify(globalStats.totalMarketCap)}
           />{" "}
         </Col>
         <Col span={12}>
           {" "}
           <Statistic
             title="Total 24h Volume"
-            // value={millify(globalStats.total24hVolume)}
+            value={millify(globalStats.total24hVolume)}
           />{" "}
         </Col>
         <Col span={12}>
           {" "}
           <Statistic
             title="Total Markets"
-            // value={millify(globalStats.totalMarkets)}
+            value={millify(globalStats.totalMarkets)}
           />{" "}
         </Col>
       </Row>
--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,7 @@
 import { render } from "react-dom";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Provider } from "react-redux";
+import { store } from "./app/store";
 import App from "./App";
 import {
   Exchanges,
@@ -14,23 +16,25 @@ const rootElement = document.getElementById("root");
 
 render(
   <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<App />}>
-        <Route path="/" element={<HomePage />} />
-        <Route path="exchanges" element={<Exchanges />} />
-        <Route path="cryptocurrencies" element={<Cryptocurrencies />} />
-        <Route path="crypto/:coinId" element={<CryptoDetails />} />
-        <Route path="news" element={<News />} />
-        <Route
-          path="*"
-          element={
-            <main style={{ padding: "1rem" }}>
-              <p>There's nothing here!</p>
-            </main>
-          }
-        />
-      </Route>
-    </Routes>
+    <Provider store={store}>
+      <Routes>
+        <Route path="/" element={<App />}>
+          <Route path="/" element={<HomePage />} />
+          <Route path="exchanges" element={<Exchanges />} />
+          <Route path="cryptocurrencies" element={<Cryptocurrencies />} />
+          <Route path="crypto/:coinId" element={<CryptoDetails />} />
+          <Route path="news" element={<News />} />
+          <Route
+            path="*"
+            element={
+              <main style={{ padding: "1rem" }}>
+                <p>There's nothing here!</p>
+              </main>
+            }
+          />
+        </Route>
+      </Routes>
+    </Provider>
   </BrowserRouter>,
   rootElement
 );
